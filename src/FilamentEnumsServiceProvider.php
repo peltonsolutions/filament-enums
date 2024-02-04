18ed6f779a3fb7d58abb3d6a913d6e9f4652b832
Nathan Pelton <npelton@gmail.com>
2024-02-03 21:27:21 -0500

    Initial commit

---
+++ src/FilamentEnumsServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Foundation\Console\AboutCommand;
+use Illuminate\Support\ServiceProvider;
+
+class FilamentEnumsServiceProvider extends ServiceProvider
+{
+	public function register(): void
+	{
+
+	}
+
+	/**
+	 * Bootstrap any application services.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		AboutCommand::add('My Package - Test', fn() => ['Version' => '1.0.0']);
+	}
+}
