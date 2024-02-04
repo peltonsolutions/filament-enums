45e62ccf9331a88c1f98863e43a70396e83c5bd2
Nathan Pelton <npelton@gmail.com>
2024-02-03 22:50:03 -0500

    Updated EnumSelect methods in form components

--- src/Models/Forms/Components/EnumSelect.php
+++ src/Models/Forms/Components/EnumSelect.php
@@ -3,15 +3,42 @@
 namespace PeltonSolutions\FilamentEnums\Models\Forms\Components;
 
 use Filament\Forms\Components\Select;
+use Illuminate\Database\Eloquent\Model;
+use PeltonSolutions\LaravelEnums\Models\Enum;
+use PeltonSolutions\LaravelEnums\Models\NullableEnum;
 
 class EnumSelect extends Select
 {
 	public static function make(string $name): static
 	{
 		return parent::make($name)
-			->options(function($record) {
-				dd($record);
-			});
+					 ->options(function($record, self $component) {
+						 if ( $record instanceof Model && isset($record->getCasts()[$component->name]) ) {
+							 $classname = $record->getCasts()[$component->name];
+							 if (is_subclass_of($classname, Enum::class)) {
+								 return $classname::map();
+							 }
+						 }
+						 return [];
+					 })
+					 ->required(function($record, self $component) {
+						 if ( $record instanceof Model && isset($record->getCasts()[$component->name]) ) {
+							 $classname = $record->getCasts()[$component->name];
+							 if (is_subclass_of($classname, Enum::class) && !is_subclass_of($classname, NullableEnum::class)) {
+								 return true;
+							 }
+						 }
+						 return false;
+					 })
+					 ->in(function($record, self $component) {
+						 if ( $record instanceof Model && isset($record->getCasts()[$component->name]) ) {
+							 $classname = $record->getCasts()[$component->name];
+							 if (is_subclass_of($classname, Enum::class)) {
+								 return $classname::map();
+							 }
+						 }
+						 return [];
+					 });
 	}
 
 }
